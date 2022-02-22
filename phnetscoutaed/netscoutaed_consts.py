b0b77ee667b0dfd2b509e7f8061f930ce9d182ca
Diogo Silva <diogo.silva@devbusters.com>
2022-02-22 16:13:54 +0000

    init commit

---
+++ phnetscoutaed/netscoutaed_consts.py
@@ -0,0 +1,18 @@
+# Define your constants here
+# Configuration fields
+NETSCOUTAED_TA_CONFIG_SERVER_URL = "server_url"
+NETSCOUTAED_TA_CONFIG_API_TOKEN = "api_token"
+NETSCOUTAED_TA_CONFIG_VERIFY_SSL = "verify_server_cert"
+
+NETSCOUTAED_TA_PARAM_IP = "ip"
+
+# Rest endpoints
+NETSCOUTAED_REST_SUMMARY = '/api/aed/v3/summary/'
+NETSCOUTAED_REST_BLOCKED_IPS = '/api/aed/v3/otf/denied-hosts/'
+NETSCOUTAED_REST_ALLOWED_IPS = '/api/aed/v3/otf/allowed-hosts/'
+
+NETSCOUTAED_REST_INBOUND_ALLOWED_HOSTS = '/api/aed/v3/protection-groups/allowed-hosts/'
+NETSCOUTAED_REST_INBOUND_DENIED_HOSTS = '/api/aed/v3/protection-groups/denied-hosts/'
+
+# Messages
+NETSCOUTAED_INVALID_IP = "Parameter 'ip' failed validation"

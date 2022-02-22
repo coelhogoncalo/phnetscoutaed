3dc8e4a50a80b78fbd8bb12d1912ad848dd95ce2
Diogo Silva <diogo.silva@devbusters.com>
2022-02-22 17:44:02 +0000

    Update netscoutaed_consts.py

--- phnetscoutaed/netscoutaed_consts.py
+++ phnetscoutaed/netscoutaed_consts.py
@@ -1,4 +1,3 @@
-# Define your constants here
 # Configuration fields
 NETSCOUTAED_TA_CONFIG_SERVER_URL = "server_url"
 NETSCOUTAED_TA_CONFIG_API_TOKEN = "api_token"
@@ -8,8 +7,9 @@ NETSCOUTAED_TA_PARAM_IP = "ip"
 
 # Rest endpoints
 NETSCOUTAED_REST_SUMMARY = '/api/aed/v3/summary/'
-NETSCOUTAED_REST_BLOCKED_IPS = '/api/aed/v3/otf/denied-hosts/'
-NETSCOUTAED_REST_ALLOWED_IPS = '/api/aed/v3/otf/allowed-hosts/'
+
+NETSCOUTAED_REST_OUTBOUND_ALLOWED_IPS = '/api/aed/v3/otf/allowed-hosts/'
+NETSCOUTAED_REST_OUTBOUND_DENIED_IPS = '/api/aed/v3/otf/denied-hosts/'
 
 NETSCOUTAED_REST_INBOUND_ALLOWED_HOSTS = '/api/aed/v3/protection-groups/allowed-hosts/'
 NETSCOUTAED_REST_INBOUND_DENIED_HOSTS = '/api/aed/v3/protection-groups/denied-hosts/'
